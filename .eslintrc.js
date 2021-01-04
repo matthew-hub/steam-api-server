335cf44dc567be1c6f508c4e74166c06b56b3216
matthew-hub <mateuszr430@gmail.com>
2021-01-04 20:35:28 +0100

    add rules

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2020: true,
+    node: true
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 11
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'linebreak-style': 0,
+    'max-len': ['error', { code: 120 }],
+    'operator-linebreak': ['error', 'before'],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'never',
+        objects: 'never',
+        imports: 'never',
+        exports: 'never',
+        functions: 'never'
+      }
+    ]
+  }
+};
