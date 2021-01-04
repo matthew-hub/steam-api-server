ae427cecfcbc107b40aac4bcbaae968aff6d4ed6
matthew-hub <mateuszr430@gmail.com>
2021-01-04 19:59:50 +0100

    create server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const request = require('request');
+
+const app = express();
+
+const port = 8080; // PORT
+const STEAM_API_KEY = 'YOUR STEAM API KEY GO HERE';
+
+// recommend use 'express' and 'request'
+// steam api routers
+require('./routers/steam-api-routers')(app, request, STEAM_API_KEY);
+
+// do your own stuff
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Example app listening at http://localhost: ${port}`);
+});
