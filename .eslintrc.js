4330ba1f9bd19a17315f6173cb228918f5b390ff
matthew-hub <mateuszr430@gmail.com>
2021-01-05 22:55:54 +0100

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     // 'linebreak-style': 0,
+    'array-bracket-newline': ['error', { multiline: true }],
     'max-len': ['error', { code: 120 }],
     'operator-linebreak': ['error', 'before'],
     'comma-dangle': [
