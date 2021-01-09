aa15ef69180f78c282b01c0d71dc5be909a53973
matthew-hub <mateuszr430@gmail.com>
2021-01-09 12:28:31 +0100

    update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const STEAM_API_KEY = 'YOUR STEAM API KEY GO HERE';
 
 // recommend use 'express' and 'request'
 // steam api routers
-require('./routers/steam-api-routers')(app, request, STEAM_API_KEY);
+require('./src/routers/steam-api-routers')(app, request, STEAM_API_KEY);
 
 // do your own stuff
 app.get('/', (req, res) => {
@@ -17,5 +17,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log(`Example app listening at http://localhost: ${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
