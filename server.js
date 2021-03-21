3ec979bae685d71d4611a8262ac23d5e6c947513
matthew-hub <mateuszr430@gmail.com>
2021-03-21 20:30:40 +0100

    add html page to server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const request = require("request");
 
 const app = express();
 
-const port = 5000 ; // PORT
+const port = 5000; // PORT
 const STEAM_API_KEY = process.env.STEAM_API_KEY;
 
 // recommend use 'express' and 'request'
@@ -12,7 +12,7 @@ require("./src/routers/steam-api-routers")(app, request, STEAM_API_KEY);
 
 // do your own stuff
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.sendFile("html/index.html");
 });
 
 app.listen(process.env.PORT || port);
