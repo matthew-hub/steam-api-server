3be23be8bdc76585318b04956a8acd40a6dad29f
matthew-hub <mateuszr430@gmail.com>
2021-03-21 18:08:42 +0100

    heroku deploy

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const request = require('request');
+const express = require("express");
+const request = require("request");
 
 const app = express();
 
 const port = 8080; // PORT
-const STEAM_API_KEY = 'YOUR STEAM API KEY GO HERE';
+const STEAM_API_KEY = "77477B45984532B16B4D17249EE5C0EC";
 
 // recommend use 'express' and 'request'
 // steam api routers
-require('./src/routers/steam-api-routers')(app, request, STEAM_API_KEY);
+require("./src/routers/steam-api-routers")(app, request, STEAM_API_KEY);
 
 // do your own stuff
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
 app.listen(port, () => {
