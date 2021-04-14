85f4c8af2bd0a81c9110be1206c09e9ca7dc80f2
matthew-hub <mateuszr430@gmail.com>
2021-04-14 19:39:16 +0200

    update error send response

--- src/routers/steam-api-routers.js
+++ src/routers/steam-api-routers.js
@@ -14,12 +14,14 @@ module.exports = (app, request, apikey) => {
     // create the Steam API URL we want to use
     const url = createURL(req.params, apikey, req.query, req.originalUrl);
 
+    // eslint-disable-next-line consistent-return
     request.get(url, (error, steamHttpResponse) => {
+      res.setHeader('Content-Type', 'application/json');
+
       if (error) {
-        return res.send(error.code);
+        return res.send({ response: error.code, error: true });
       }
       // once we get the body of the steamHttpResponse, send it to our client
-      res.setHeader('Content-Type', 'application/json');
       res.send(steamHttpResponse.body);
     });
   });
@@ -27,13 +29,14 @@ module.exports = (app, request, apikey) => {
   app.get('/steam/:method/:id/:appid/', (req, res) => {
     // create the Steam API URL we want to use
     const url = createURL(req.params, apikey, req.query, req.originalUrl);
-
+    // eslint-disable-next-line consistent-return
     request.get(url, (error, steamHttpResponse) => {
+      res.setHeader('Content-Type', 'application/json');
+
       if (error) {
-        return res.send(error.code);
+        return res.send({ response: error.code, error: true });
       }
       // once we get the body of the steamHttpResponse, send it to our client
-      res.setHeader('Content-Type', 'application/json');
       res.send(steamHttpResponse.body);
     });
   });
